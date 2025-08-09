3c3dccddc98c889ad09db3ad0e142ba781107992
Marxalone <officialsamuelsunny@gmail.com>
2025-08-09 11:27:06 +0100

    Add files via upload

--- api/server.js
+++ api/server.js
@@ -27,8 +27,8 @@ const CONFIG = {
     enable: true
   },
   RATE_LIMIT: {
-    windowMs: 15 * 60 * 1000,     // 15 minutes
-    max: 100                       // limit each IP to 100 requests per windowMs
+    windowMs: 95 * 60 * 1000,     // 15 minutes
+    max: 10000                       // limit each IP to 100 requests per windowMs
   }
 };
 
